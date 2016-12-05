6ba0322a2d2f1d6126b49b76846a1a3ff45fffb1
Nick Dant <n.dant@utexas.edu>
2016-12-04 21:56:52 -0600

    Script is more general now for loans

--- amortization.py
+++ amortization.py
@@ -4,9 +4,9 @@ from tabulate import tabulate
 
 PURCHASE_PRICE = 0
 DOWN_PAYMENT_PERCENT = 0
-INITIAL_PRINCIPAL_AMT = 0 #after down payment
+INITIAL_PRINCIPAL_AMT = 0 #after down payment, if applicable
 INTEREST_RATE =  0 #percentage
-MORTGAGE_LENGTH = 0 #years
+LOAN_LENGTH = 0 #years
 
 
 
@@ -15,19 +15,19 @@ def main():
     global DOWN_PAYMENT_PERCENT
     global INITIAL_PRINCIPAL_AMT
     global INTEREST_RATE
-    global MORTGAGE_LENGTH
+    global LOAN_LENGTH
     PURCHASE_PRICE = float(raw_input("Enter purchase price: "))
     DOWN_PAYMENT_PERCENT = float(raw_input("Enter down payment %: "))
     down_payment = PURCHASE_PRICE * (DOWN_PAYMENT_PERCENT/100)
     INTEREST_RATE = float(raw_input("Enter interest rate %: "))
     INTEREST_RATE = INTEREST_RATE/100
-    MORTGAGE_LENGTH = int(raw_input("Enter mortgage length: "))
+    LOAN_LENGTH = int(raw_input("Enter loan length: "))
     INITIAL_PRINCIPAL_AMT = PURCHASE_PRICE - down_payment
 
     schedule = []
     beginning_balance = INITIAL_PRINCIPAL_AMT
     pmt = calculate_pmt(beginning_balance)
-    for i in range(0,MORTGAGE_LENGTH*12):
+    for i in range(0,LOAN_LENGTH*12):
         new_row = []
         new_row.append(i + 1) #month
         new_row.append(beginning_balance)
@@ -37,7 +37,7 @@ def main():
         principal = pmt - interest
         new_row.append(principal)
         ending_balance = beginning_balance - principal
-        new_row.append(ending_balance) #ending balance
+        new_row.append(ending_balance)
         schedule.append(new_row)
         beginning_balance = ending_balance
 
@@ -46,7 +46,7 @@ def main():
 
 
 def calculate_pmt(present_value):
-    numerator = 1 - (1/ ((1+(INTEREST_RATE/12))**(MORTGAGE_LENGTH*12)))
+    numerator = 1 - (1/ ((1+(INTEREST_RATE/12))**(LOAN_LENGTH*12)))
     denominator = INTEREST_RATE/12
 
     return present_value / (numerator/denominator)
